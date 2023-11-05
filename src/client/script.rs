4160605d484bf149fc5d5a2c765edc1fbfa9c272
Sanket <sanket143@users.noreply.github.com>
2023-11-05 16:29:05 +0530

    run tests with node version of the host

--- src/client/script.rs
+++ src/client/script.rs
@@ -1,32 +1,12 @@
 use anyhow::Result;
-use colored_json::ToColoredJson;
-use deno_core::*;
-use deno_core::{op, Extension, JsRuntime, RuntimeOptions};
-
-#[op]
-fn op_fail(text: &str) {
-    println!("\x1b[31m{}\x1b[0m", text);
-}
-
-#[op]
-fn op_pass(text: &str) {
-    println!("\x1b[32m{}\x1b[0m", text);
-}
-
-#[op]
-fn op_json_print(text: &str) {
-    println!("{}", text.to_colored_json_auto().unwrap());
-}
+use std::process::Command;
 
 pub fn execute(js_code: String) -> Result<String> {
-    let ext = Extension::builder("my_ext")
-        .ops(vec![op_fail::DECL, op_pass::DECL, op_json_print::DECL])
-        .build();
-    let mut js_runtime = JsRuntime::new(RuntimeOptions {
-        extensions: vec![ext],
-        ..RuntimeOptions::default()
-    });
-    js_runtime.execute_script("<anon>", js_code.into())?;
+    Command::new("node")
+        .arg("-p")
+        .arg(js_code)
+        .spawn()
+        .expect("node command failed to start");
 
     Ok(String::new())
 }
